b874fa3f2ca735e129174bf671ac8737e0d6bdb0
abinash sonowal <abinashrasonowal@gmail.com>
2024-11-30 08:27:03 +0530

    na

--- using_inbuild_library.py
+++ using_inbuild_library.py
@@ -14,8 +14,6 @@ df = get_clean_dataset()
 X = df.iloc[:,:-1]
 y = df.iloc[:, -1]
 
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 sc = StandardScaler()
 X_train_sc = sc.fit_transform(X_train)
@@ -52,6 +50,7 @@ y_svm = svm_classifier.predict(X_test)
 print("----------SVM unscaled --------")
 print("SVM Classifier Score:", svm_classifier.score(X_test, y_test))
 print("Mean Absolute Error:", mean_absolute_error(y_test, y_svm))
+print("Accuracy:", accuracy_score(y_test, y_svm))
 
 # Create SVM Classifier scaled
 svm_classifier2 = SVC(decision_function_shape='ovr') 
@@ -61,15 +60,28 @@ y_svm = svm_classifier2.predict(X_test_sc)
 print("----------SVM scaled ---------")
 print("SVM Classifier Score:", svm_classifier2.score(X_test_sc, y_test))
 print("Mean Absolute Error:", mean_absolute_error(y_test, y_svm))
+print("Accuracy:", accuracy_score(y_test, y_svm))
 
 
 # ----------------- Removing outlier using Kmean ----------------
 indicators = df.columns[:-1]
-km = KMeans(n_clusters = 2)
+km = KMeans(n_clusters = 6)
 km.fit(df[indicators])
 labels = km.labels_
-df_km = df[labels == 1]
-df_km.reset_index(inplace = True)
+# df_km = df[labels == 1]
+# df_km.reset_index(inplace = True)
+
+# Calculate distances to cluster centroids
+centroids = km.cluster_centers_
+distances = np.linalg.norm(df[indicators].values - centroids[labels], axis=1)
+
+# Set a threshold for outliers
+threshold = np.percentile(distances, 95)  # Adjust threshold as needed
+
+# Filter outliers
+df_km = df[distances <= threshold]
+df_km.reset_index(inplace=True)
+
 
 from sklearn.preprocessing  import LabelEncoder
 le = LabelEncoder()
@@ -87,10 +99,12 @@ lr = LogisticRegression()
 lr.fit(X_train_sc, y_train)
 y_lr = lr.predict(X_test_sc)
 
+print("\n\n")
 # Model Score
 print("------------------LR ----------")
 print("Normal Score :", lr.score(X_test_sc, y_test))
 print("Mean Absolute Error :", mean_absolute_error(y_test, y_lr))
+print("Accuracy:", accuracy_score(y_test, y_lr))
 conf_matrix = confusion_matrix(y_test, y_lr)
 print("Confusion Matrix:\n", conf_matrix)
 
@@ -102,5 +116,6 @@ y_svm = svm_classifier2.predict(X_test_sc)
 print("---------------SVM----------------")
 print("SVM Classifier Score:", svm_classifier2.score(X_test_sc, y_test))
 print("Mean Absolute Error:", mean_absolute_error(y_test, y_svm))
+print("Accuracy:", accuracy_score(y_test, y_svm))
 conf_matrix = confusion_matrix(y_test, y_svm)
 print("Confusion Matrix:\n", conf_matrix)
