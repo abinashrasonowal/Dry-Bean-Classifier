47bd22c21c020ab7509f38514cb78a04d2be3551
abinash sonowal <abinashrasonowal@gmail.com>
2024-12-01 10:15:31 +0530

    Update custom_libraries.py

--- custom_libraries.py
+++ custom_libraries.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from utils import *
 
 class LogisticRegressionMulticlass:
